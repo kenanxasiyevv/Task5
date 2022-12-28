c34c1c6f6cc82c69793f01dd67685a7fd6df9377
kenanxasiyevv <kananakh@code.edu.az>
2022-12-28 20:40:54 +0400

    Add project files.

---
+++ Task4/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Task4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Eded daxil et: ");
+            int number = int.Parse(Console.ReadLine());
+            Console.Write("Eded daxil et: ");
+            int number2 = int.Parse(Console.ReadLine());
+            int sum = 0;
+            for (int r =(int) Math.Sqrt(number); r <= Math.Sqrt(number2); r++)
+            {
+                if (number2 >= r * r && r*r>=number)
+                    sum++;
+
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}              
